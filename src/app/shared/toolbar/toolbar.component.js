6d786fe8c3c8e4c5c5defe9116e6d4220cb46cb4
u4bi <rootcode10@gmail.com>
2018-01-06 11:47:10 +0900

    Added withRouter for connected component

--- src/app/shared/toolbar/toolbar.component.js
+++ src/app/shared/toolbar/toolbar.component.js
@@ -1,71 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './toolbar.component.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 import { Menu, Button } from 'semantic-ui-react';
 
-export default function ToolbarComponent(){
+class ToolbarComponent extends Component {
 
-    var flag = false;
-
-    var onRoute = () => {
-    };
-
-    var onToggle = () => Array.from(document.querySelectorAll('.Toolbar__Toggle')).map(e => {
-        
-        window.getComputedStyle(e, 'style').getPropertyValue('display') === 'none' ? [e.style.display = 'flex', flag = true] : [e.style.display = 'none', , flag = false];
-
-    });
-
-    window.addEventListener('resize', () => {
-        if(window.innerWidth < 768 && flag) {
-            onToggle();
-            
-        }
-
-        if(window.innerWidth > 768 && !flag) {
-            onToggle();
-            
-        }
-    });
+    flag = false;
     
-    return (
-        <div className='Toolbar'>
-            <Menu pointing stackable>
-                <Menu.Item name='main'>
-                    <NavLink className='Toolbar__Title' to='/' exact activeClassName='active'>
-                        <span>
-                            <strong>DCleaner</strong> for 디시인사이드
-                        </span>
-                    </NavLink>
+    onRoute = (e, { name }) => {
+        this.props.history.push(name);
+        if(window.innerWidth < 768 && this.flag) this.onToggle();
+    }
 
-                    <div className='Toolbar__Toggle__Btn__Wrapper'>
-                        <Button circular content='열기' className='Toolbar__Toggle__Btn' onClick={ onToggle } />
-                    </div>
+    onToggle = () => Array.from(document.querySelectorAll('.Toolbar__Toggle')).map(e => window.getComputedStyle(e, 'style').getPropertyValue('display') === 'none' ? [e.style.display = 'flex', this.flag = true] : [e.style.display = 'none', , this.flag = false] );
 
-                </Menu.Item>
+    constructor(){
 
-                <Menu.Menu position='left' className='Toolbar__Toggle'>
-                    <Menu.Item name='info' className='Toolbar__Menu' onClick={ onRoute }>
-                        특징
-                    </Menu.Item>
-                    <Menu.Item name='price' className='Toolbar__Menu' onClick={ onRoute }>
-                        가격정책
-                    </Menu.Item>
-                    <Menu.Item name='support' className='Toolbar__Menu' onClick={ onRoute }>
-                        문의지원
-                    </Menu.Item>
-                </Menu.Menu>
+        super();
+        
+        window.addEventListener('resize', () => {
+            if(window.innerWidth < 768 && this.flag) this.onToggle();
+            if(window.innerWidth > 768 && !this.flag) this.onToggle();
+        });
+    }
 
-                <Menu.Menu position='right' className='Toolbar__Toggle'>
-                    <Menu.Item name='login' className='Toolbar__Menu' onClick={ onRoute }>
-                        로그인
+    render() {
+        return (
+            <div className='Toolbar'>
+                <Menu pointing stackable>
+                    <Menu.Item name='main'>
+                        <NavLink className='Toolbar__Title' to='/' exact activeClassName='active'>
+                            <span>
+                                <strong>DCleaner</strong> for 디시인사이드
+                            </span>
+                        </NavLink>
+
+                        <div className='Toolbar__Toggle__Btn__Wrapper'>
+                            <Button circular content='열기' className='Toolbar__Toggle__Btn' onClick={ this.onToggle } />
+                        </div>
+
                     </Menu.Item>
 
-                    <Menu.Item name='regist' className='Toolbar__Menu' onClick={ onRoute }>
-                        결제등록
-                    </Menu.Item>
-                </Menu.Menu>
-            </Menu>
-        </div>
-    );
+                    <Menu.Menu position='left' className='Toolbar__Toggle'>
+                        <Menu.Item name='info' className='Toolbar__Menu' onClick={ this.onRoute }>
+                            특징
+                        </Menu.Item>
+                        <Menu.Item name='price' className='Toolbar__Menu' onClick={ this.onRoute }>
+                            가격정책
+                        </Menu.Item>
+                        <Menu.Item name='support' className='Toolbar__Menu' onClick={ this.onRoute }>
+                            문의지원
+                        </Menu.Item>
+                    </Menu.Menu>
+
+                    <Menu.Menu position='right' className='Toolbar__Toggle'>
+                        <Menu.Item name='login' className='Toolbar__Menu' onClick={ this.onRoute }>
+                            로그인
+                        </Menu.Item>
+
+                        <Menu.Item name='regist' className='Toolbar__Menu' onClick={ this.onRoute }>
+                            결제등록
+                        </Menu.Item>
+                    </Menu.Menu>
+                </Menu>
+            </div>
+        );
+    }
 }
+
+export default withRouter(ToolbarComponent);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import AppComponent from './app/app.component';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<AppComponent />, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
