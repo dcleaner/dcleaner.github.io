1e352885081bf07fea1e2b9b3774bdb02b6ecc54
u4bi <rootcode10@gmail.com>
2018-01-06 23:40:16 +0900

    Added Main-header Stylesheet

--- src/app/pages/main/main-header/main-header-image/main-header-image.component.js
+++ src/app/pages/main/main-header/main-header-image/main-header-image.component.js
@@ -2,9 +2,35 @@ import React from 'react';
 import './main-header-image.component.css';
 
 export default function MainHeaderImageComponent(){
+
+    var demos = [
+        'https://raw.githubusercontent.com/u4bi/dc-cleaner2/master/assets/search.gif',
+        'https://raw.githubusercontent.com/u4bi/dc-cleaner2/master/assets/gallog.gif',
+        'https://raw.githubusercontent.com/u4bi/dc-cleaner2/master/assets/search_reply.gif',
+        'https://raw.githubusercontent.com/u4bi/dc-cleaner2/master/assets/gall_reply.gif'
+    ];
+
+    var selectedDemo = 0;
+
+    var divStyle = {
+        backgroundImage : `url(${ demos[selectedDemo] })`
+    };
+
+    var onDemo = () => {
+        selectedDemo !== 3 ? selectedDemo++ : selectedDemo = 0;
+        document.querySelector('.MainHeader__Image__viewport').style.backgroundImage = `url(${ demos[selectedDemo]})`;
+    };
+
     return (
         <div className='MainHeaderImage'>
-            <h1>MainHeaderImage</h1>
+            <div className='MainHeader__Image__imac'>
+            <div className='MainHeader__Image__screen'>
+                <div className='MainHeader__Image__viewport' style={ divStyle } onClick={ onDemo }></div>
+            </div>
+            <div className='MainHeader__Image__logo'></div>
+            <div className='MainHeader__Image__leg'></div>
+            <div className='MainHeader__Image__foot'></div>
+            </div>
         </div>
     );
 }
--- src/app/pages/main/main-header/main-header-text/main-header-text.component.js
+++ src/app/pages/main/main-header/main-header-text/main-header-text.component.js
@@ -4,7 +4,12 @@ import './main-header-text.component.css';
 export default function MainHeaderTextComponent(){
     return (
         <div className='MainHeaderText'>
-            <h1>MainHeaderText</h1>
+            <div className='MainHeaderText__Wrapper'>
+                <h1>매번 막히던 클리너는 이제 안녕!</h1>
+                <div className='MainHeaderText__Wrapper__Sub'>
+                    <h3>Chrome Extension으로 새롭게 탈바꿈한 디시클리너가 갤러리부터 갤로그까지 단번에 모두 정리해드립니다.</h3>
+                </div>
+            </div>
         </div>
     );
 }
