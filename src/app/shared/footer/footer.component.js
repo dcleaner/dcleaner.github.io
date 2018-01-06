483537d2861622f788f0f1ce7103097a2bce3fb3
u4bi <rootcode10@gmail.com>
2018-01-06 19:45:47 +0900

    Added Footer Stylesheet

--- src/app/shared/footer/footer.component.js
+++ src/app/shared/footer/footer.component.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './footer.component.css';
+import { Grid } from 'semantic-ui-react';
+
+import logo from '../../../assets/dcleaner.png';
 
 export default function FooterComponent(){
     return (
         <div className='Footer'>
-            <h1>Footer</h1>
+            <Grid centered={ true }>
+                <Grid.Column mobile={ 16 } tablet={ 12 } computer={ 13 }>
+                    <div className='Footer__Grid Text' >
+                        Copyright © 2018 Dcleaner. All Rights Reserved.
+                    </div>
+                </Grid.Column>
+                <Grid.Column mobile={ 16 } tablet={ 4 } computer={ 3 }>
+                    <div className='Footer__Grid Image' >
+                        <img className='Footer__Logo' src={ logo } />
+                    </div>
+                </Grid.Column>                
+            </Grid>
         </div>
     );
 }
