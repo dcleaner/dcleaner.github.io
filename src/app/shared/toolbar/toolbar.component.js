bf25f889551d81a3a6a1a0b3df1ca10c0be733ea
u4bi <rootcode10@gmail.com>
2018-01-06 10:22:22 +0900

    Added Mobile Toolbar Stylesheet

--- src/app/shared/toolbar/toolbar.component.js
+++ src/app/shared/toolbar/toolbar.component.js
@@ -1,35 +1,46 @@
 import React from 'react';
 import './toolbar.component.css';
 import { NavLink } from 'react-router-dom';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Button } from 'semantic-ui-react';
 
 export default function ToolbarComponent(){
+
+    var flag = false;
 
     var onRoute = () => {
     };
 
+    var onToggle = () => Array.from(document.querySelectorAll('.Toolbar__Toggle')).map(e => e.style.display = window.getComputedStyle(e, 'style').getPropertyValue('display') === 'none' ? 'flex' : 'none');
+
     return (
         <div className='Toolbar'>
-            <Menu pointing>
+            <Menu pointing stackable>
                 <Menu.Item name='main'>
                     <NavLink className='Toolbar__Title' to='/' exact activeClassName='active'>
                         <span>
                             <strong>DCleaner</strong> for 디시인사이드
                         </span>
                     </NavLink>
-                </Menu.Item>
-                <Menu.Item name='info' className='Toolbar__Menu' onClick={ onRoute }>
-                    특징
-                </Menu.Item>
-                <Menu.Item name='price' className='Toolbar__Menu' onClick={ onRoute }>
-                    가격정책
-                </Menu.Item>
-                <Menu.Item name='support' className='Toolbar__Menu' onClick={ onRoute }>
-                    문의지원
+
+                    <div className='Toolbar__Toggle__Btn__Wrapper'>
+                        <Button circular content='열기' className='Toolbar__Toggle__Btn' onClick={ onToggle } />
+                    </div>
+
                 </Menu.Item>
 
-                <Menu.Menu position='right'>
-                    
+                <Menu.Menu position='left' className='Toolbar__Toggle'>
+                    <Menu.Item name='info' className='Toolbar__Menu' onClick={ onRoute }>
+                        특징
+                    </Menu.Item>
+                    <Menu.Item name='price' className='Toolbar__Menu' onClick={ onRoute }>
+                        가격정책
+                    </Menu.Item>
+                    <Menu.Item name='support' className='Toolbar__Menu' onClick={ onRoute }>
+                        문의지원
+                    </Menu.Item>
+                </Menu.Menu>
+
+                <Menu.Menu position='right' className='Toolbar__Toggle'>
                     <Menu.Item name='login' className='Toolbar__Menu' onClick={ onRoute }>
                         로그인
                     </Menu.Item>
@@ -37,7 +48,6 @@ export default function ToolbarComponent(){
                     <Menu.Item name='regist' className='Toolbar__Menu' onClick={ onRoute }>
                         결제등록
                     </Menu.Item>
-
                 </Menu.Menu>
             </Menu>
         </div>
