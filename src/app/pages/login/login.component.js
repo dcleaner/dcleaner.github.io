3f51e47566671a6bdf1317b70ce201c81bf69aa5
u4bi <rootcode10@gmail.com>
2018-01-07 18:26:39 +0900

    Added Login Stylesheet

--- src/app/pages/login/login.component.js
+++ src/app/pages/login/login.component.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './login.component.css';
+import { Search } from 'semantic-ui-react';
 
 export default function LoginComponent(){
+    
+    setTimeout(() => document.querySelector('input').focus(), 50);
+
     return (
         <div className='Login'>
-            <h1>Login</h1>
+            <div className='Login__Input'>
+                <Search 
+                    noResultsMessage='등록된 정보가 없습니다.'
+                />
+            </div>
         </div>
     );
 }
